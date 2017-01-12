375dc208f4089f3f04021fc6779b19e2dd1bc692
Robert Lord <robert@lord.io>
2017-01-11 22:03:09 -0600

    Lesson 1, part 2

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn main() {
 }
 
 fn line(img: &mut Img, x0: u32, y0: u32, x1: u32, y1: u32) {
-    let mut t = 0.0;
-    while t < 1.0 {
+    for i in x0..x1 {
+        let mut t = ((i-x0) as f64)/((x1-x0) as f64);
         let x = x0 as f64 * (1.0-t) + x1 as f64 * t;
         let y = y0 as f64 * (1.0-t) + y1 as f64 * t;
         img.put(x as u32, y as u32, 255, 255, 255);
