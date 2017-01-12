de108783a84d4d666d85c4e1ed37ebc01f2138f4
Robert Lord <robert@lord.io>
2017-01-11 21:57:48 -0600

    Lesson 1, part 1

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ use img::Img;
 
 fn main() {
     let mut img = Img::new();
-    img.put(10, 10, 100, 100, 100);
+    line(&mut img, 5, 10, 800, 29);
     img.save();
+}
+
+fn line(img: &mut Img, x0: u32, y0: u32, x1: u32, y1: u32) {
+    let mut t = 0.0;
+    while t < 1.0 {
+        let x = x0 as f64 * (1.0-t) + x1 as f64 * t;
+        let y = y0 as f64 * (1.0-t) + y1 as f64 * t;
+        img.put(x as u32, y as u32, 255, 255, 255);
+        t += 0.01;
+    }
 }
